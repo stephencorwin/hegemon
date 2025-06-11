299dcba6dfe58366dbe20091215fa6f2001ea7b6
Stephen Corwin <stephenjcorwin@gmail.com>
2025-06-11 09:20:40 -0400

    Updated macros

--- app/mock/macros.ts
+++ app/mock/macros.ts
@@ -9,28 +9,8 @@ import {
 
 // use https://www.wowhead.com/icons for placeholder icons
 
-export const MACRO_FIRE_SHIELD: IMacro = {
-  id: '1',
-  name: 'Fire Shield',
-  icon: 'https://wow.zamimg.com/images/wow/icons/large/inv_shield_05.jpg',
-  description: 'Create a small in-the-money bullish position.',
-  strategyType: STRATEGY_TYPE.BULL,
-  legs: [
-    {
-      action: MACRO_ACTION.BUY,
-      orderClass: ORDER_CLASS.OPTION,
-      optionType: OPTION_TYPE.CALL,
-      optionTranche: -1,
-      quantity: 1,
-      automation: [
-        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
-      ],
-    },
-  ],
-};
-
 export const MACRO_FLARE: IMacro = {
-  id: '2',
+  id: '1',
   name: 'Flare',
   icon: 'https://wow.zamimg.com/images/wow/icons/large/inv_elemental_mote_fire01.jpg',
   description:
@@ -51,7 +31,7 @@ export const MACRO_FLARE: IMacro = {
 };
 
 export const MACRO_FIREBALL: IMacro = {
-  id: '3',
+  id: '2',
   name: 'Fireball',
   icon: 'https://wow.zamimg.com/images/wow/icons/large/spell_fire_fireball02.jpg',
   description:
@@ -72,7 +52,7 @@ export const MACRO_FIREBALL: IMacro = {
 };
 
 export const MACRO_EXPLOSION: IMacro = {
-  id: '4',
+  id: '3',
   name: 'Explosion',
   icon: 'https://wow.zamimg.com/images/wow/icons/large/spell_shaman_improvedfirenova.jpg',
   description:
@@ -93,7 +73,7 @@ export const MACRO_EXPLOSION: IMacro = {
 };
 
 export const MACRO_FIRE_STORM: IMacro = {
-  id: '5',
+  id: '4',
   name: 'Fire Storm',
   icon: 'https://wow.zamimg.com/images/wow/icons/large/spell_shadow_rainoffire.jpg',
   description:
@@ -133,18 +113,19 @@ export const MACRO_FIRE_STORM: IMacro = {
   ],
 };
 
-export const MACRO_FROST_SHIELD: IMacro = {
-  id: '6',
-  name: 'Frost Shield',
-  icon: 'https://wow.zamimg.com/images/wow/icons/large/spell_frost_frostarmor02.jpg',
-  description: 'Create a small in-the-money bearish position.',
-  strategyType: STRATEGY_TYPE.BEAR,
+export const MACRO_FIRE_TICKET: IMacro = {
+  id: '5',
+  name: 'Fire Ticket',
+  icon: 'https://wow.zamimg.com/images/wow/icons/large/inv_10_inscription_darkmooncards_fire_blank.jpg',
+  description:
+    'Create a far out of the money bullish position to capture extreme momentum. High risk.',
+  strategyType: STRATEGY_TYPE.BULL,
   legs: [
     {
       action: MACRO_ACTION.BUY,
       orderClass: ORDER_CLASS.OPTION,
-      optionType: OPTION_TYPE.PUT,
-      optionTranche: +1,
+      optionType: OPTION_TYPE.CALL,
+      optionTranche: +5,
       quantity: 1,
       automation: [
         {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
@@ -154,7 +135,7 @@ export const MACRO_FROST_SHIELD: IMacro = {
 };
 
 export const MACRO_FROST_BOLT: IMacro = {
-  id: '7',
+  id: '6',
   name: 'Frostbolt',
   icon: 'https://wow.zamimg.com/images/wow/icons/large/spell_frost_frostbolt02.jpg',
   description:
@@ -175,7 +156,7 @@ export const MACRO_FROST_BOLT: IMacro = {
 };
 
 export const MACRO_ICE_LANCE: IMacro = {
-  id: '8',
+  id: '7',
   name: 'Ice Lance',
   icon: 'https://wow.zamimg.com/images/wow/icons/large/spell_frost_iceshard.jpg',
   description:
@@ -196,7 +177,7 @@ export const MACRO_ICE_LANCE: IMacro = {
 };
 
 export const MACRO_BLACK_HOLE: IMacro = {
-  id: '9',
+  id: '8',
   name: 'Black Hole',
   icon: 'https://wow.zamimg.com/images/wow/icons/large/inv_engineering_90_blackhole.jpg',
   description:
@@ -217,7 +198,7 @@ export const MACRO_BLACK_HOLE: IMacro = {
 };
 
 export const MACRO_BLIZZARD: IMacro = {
-  id: '10',
+  id: '9',
   name: 'Blizzard',
   icon: 'https://wow.zamimg.com/images/wow/icons/large/spell_frost_icestorm.jpg',
   description:
@@ -250,6 +231,27 @@ export const MACRO_BLIZZARD: IMacro = {
       optionType: OPTION_TYPE.PUT,
       optionTranche: -4,
       quantity: 2,
+      automation: [
+        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
+      ],
+    },
+  ],
+};
+
+export const MACRO_ICE_TICKET: IMacro = {
+  id: '10',
+  name: 'Ice Ticket',
+  icon: 'https://wow.zamimg.com/images/wow/icons/large/inv_10_inscription_darkmooncards_frost_blank.jpg',
+  description:
+    'Create a far out of the money bearish position to capture extreme momentum. High risk.',
+  strategyType: STRATEGY_TYPE.BEAR,
+  legs: [
+    {
+      action: MACRO_ACTION.BUY,
+      orderClass: ORDER_CLASS.OPTION,
+      optionType: OPTION_TYPE.PUT,
+      optionTranche: -5,
+      quantity: 1,
       automation: [
         {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
       ],
@@ -402,16 +404,16 @@ export const MACRO_BLIZZARD: IMacro = {
 // };
 
 export const MACROS = {
-  [MACRO_FIRE_SHIELD.id]: MACRO_FIRE_SHIELD,
   [MACRO_FLARE.id]: MACRO_FLARE,
   [MACRO_FIREBALL.id]: MACRO_FIREBALL,
   [MACRO_EXPLOSION.id]: MACRO_EXPLOSION,
   [MACRO_FIRE_STORM.id]: MACRO_FIRE_STORM,
-  [MACRO_FROST_SHIELD.id]: MACRO_FROST_SHIELD,
+  [MACRO_FIRE_TICKET.id]: MACRO_FIRE_TICKET,
   [MACRO_FROST_BOLT.id]: MACRO_FROST_BOLT,
   [MACRO_ICE_LANCE.id]: MACRO_ICE_LANCE,
   [MACRO_BLIZZARD.id]: MACRO_BLIZZARD,
   [MACRO_BLACK_HOLE.id]: MACRO_BLACK_HOLE,
+  [MACRO_ICE_TICKET.id]: MACRO_ICE_TICKET,
   // [MACRO_STRADDLE.id]: MACRO_STRADDLE,
   // [MACRO_STRANGLE.id]: MACRO_STRANGLE,
   // [MACRO_BUTTERFLY.id]: MACRO_BUTTERFLY,
