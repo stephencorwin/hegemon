97f3e024652a54e0c4eb811f972fe1f4d6e3eb59
Stephen Corwin <stephenjcorwin@gmail.com>
2025-06-20 12:43:26 -0400

    Updated cache

--- app/mock/defaultProfile.ts
+++ app/mock/defaultProfile.ts
@@ -16,7 +16,7 @@ export const DEFAULT_PROFILE: IHegemonStore['profile'] = {
       'HOOD',
       'UNH',
     ],
-    optionsExpiration: '2025-06-20',
+    optionsExpiration: '2025-06-27',
     thresholds: {
       profit: {
         dailyGoal: 1.1,
