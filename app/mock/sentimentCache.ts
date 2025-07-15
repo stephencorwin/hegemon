234becc7b08df76585bd92bcea127e690d352996
Stephen Corwin <stephenjcorwin@gmail.com>
2025-07-15 17:38:34 -0400

    Updated sentiment cache

--- app/mock/sentimentCache.ts
+++ app/mock/sentimentCache.ts
@@ -1,10 +1,10 @@
 export const SENTIMENT_CACHE = {
-  GOOGL: 0.327,
-  SPY: 0.012,
-  NVDA: 0.294,
-  AMD: 0.499,
-  SMCI: 0.736,
-  PLTR: 0.509,
-  TSLA: 0.167,
-  UNH: 0.274,
+  GOOGL: 0.398,
+  SPY: 0.003,
+  NVDA: 0.286,
+  AMD: 0.469,
+  SMCI: 0.702,
+  PLTR: 0.421,
+  TSLA: 0.142,
+  UNH: 0.267,
 };
