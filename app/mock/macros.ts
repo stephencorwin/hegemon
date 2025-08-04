27feb7ab4d23d292b3781a0d8ba703d90ed580f6
Stephen Corwin <stephenjcorwin@gmail.com>
2025-08-04 11:54:36 -0400

    Updated default macros

--- app/mock/macros.ts
+++ app/mock/macros.ts
@@ -23,7 +23,7 @@ export const MACRO_FLARE: IMacro = {
       optionTranche: -2,
       quantity: 1,
       automation: [
-        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
+        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.14},
       ],
     },
   ],
@@ -43,17 +43,17 @@ export const MACRO_FIREBALL: IMacro = {
       optionTranche: -1,
       quantity: 1,
       automation: [
-        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
+        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.14},
       ],
     },
   ],
 };
 
-export const MACRO_EXPLOSION: IMacro = {
+export const MACRO_FIRE_STORM: IMacro = {
   id: '3',
-  name: 'Explosion',
-  icon: 'https://wow.zamimg.com/images/wow/icons/large/spell_shaman_improvedfirenova.jpg',
-  description: 'Capture heavy upward momentum. Medium risk.',
+  name: 'Fire Storm',
+  icon: 'https://wow.zamimg.com/images/wow/icons/large/spell_shadow_rainoffire.jpg',
+  description: 'Capture upward momentum. Medium risk.',
   strategyType: STRATEGY_TYPE.BULL,
   legs: [
     {
@@ -63,14 +63,34 @@ export const MACRO_EXPLOSION: IMacro = {
       optionTranche: +1,
       quantity: 1,
       automation: [
-        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
+        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.14},
+      ],
+    },
+  ],
+};
+
+export const MACRO_EXPLOSION: IMacro = {
+  id: '4',
+  name: 'Explosion',
+  icon: 'https://wow.zamimg.com/images/wow/icons/large/spell_shaman_improvedfirenova.jpg',
+  description: 'Capture heavy upward momentum. High risk.',
+  strategyType: STRATEGY_TYPE.BULL,
+  legs: [
+    {
+      action: MACRO_ACTION.BUY,
+      orderClass: ORDER_CLASS.OPTION,
+      optionType: OPTION_TYPE.CALL,
+      optionTranche: +2,
+      quantity: 1,
+      automation: [
+        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.14},
       ],
     },
   ],
 };
 
 export const MACRO_FIRE_TICKET: IMacro = {
-  id: '4',
+  id: '5',
   name: 'Fire Ticket',
   icon: 'https://wow.zamimg.com/images/wow/icons/large/inv_10_inscription_darkmooncards_fire_blank.jpg',
   description: 'Capture extreme upward momentum. High risk.',
@@ -80,41 +100,10 @@ export const MACRO_FIRE_TICKET: IMacro = {
       action: MACRO_ACTION.BUY,
       orderClass: ORDER_CLASS.OPTION,
       optionType: OPTION_TYPE.CALL,
-      optionTranche: +2,
+      optionTranche: +3,
       quantity: 1,
       automation: [
-        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
-      ],
-    },
-  ],
-};
-
-export const MACRO_FIRE_STORM: IMacro = {
-  id: '5',
-  name: 'Fire Storm',
-  icon: 'https://wow.zamimg.com/images/wow/icons/large/spell_shadow_rainoffire.jpg',
-  description:
-    'Create a series of bullish positions to dilute risk while capturing upward momentum. Medium risk.',
-  strategyType: STRATEGY_TYPE.BULL,
-  legs: [
-    {
-      action: MACRO_ACTION.BUY,
-      orderClass: ORDER_CLASS.OPTION,
-      optionType: OPTION_TYPE.CALL,
-      optionTranche: -2,
-      quantity: 1,
-      automation: [
-        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
-      ],
-    },
-    {
-      action: MACRO_ACTION.BUY,
-      orderClass: ORDER_CLASS.OPTION,
-      optionType: OPTION_TYPE.CALL,
-      optionTranche: +2,
-      quantity: 1,
-      automation: [
-        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
+        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.14},
       ],
     },
   ],
@@ -134,7 +123,7 @@ export const MACRO_FROST_BOLT: IMacro = {
       optionTranche: +2,
       quantity: 1,
       automation: [
-        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
+        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.14},
       ],
     },
   ],
@@ -154,17 +143,17 @@ export const MACRO_ICE_LANCE: IMacro = {
       optionTranche: +1,
       quantity: 1,
       automation: [
-        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
+        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.14},
       ],
     },
   ],
 };
 
-export const MACRO_BLACK_HOLE: IMacro = {
+export const MACRO_BLIZZARD: IMacro = {
   id: '8',
-  name: 'Black Hole',
-  icon: 'https://wow.zamimg.com/images/wow/icons/large/inv_engineering_90_blackhole.jpg',
-  description: 'Capture heavy downward momentum. Medium risk.',
+  name: 'Blizzard',
+  icon: 'https://wow.zamimg.com/images/wow/icons/large/spell_frost_icestorm.jpg',
+  description: 'Capture downward momentum. Medium risk.',
   strategyType: STRATEGY_TYPE.BEAR,
   legs: [
     {
@@ -174,14 +163,34 @@ export const MACRO_BLACK_HOLE: IMacro = {
       optionTranche: -1,
       quantity: 1,
       automation: [
-        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
+        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.14},
+      ],
+    },
+  ],
+};
+
+export const MACRO_BLACK_HOLE: IMacro = {
+  id: '9',
+  name: 'Black Hole',
+  icon: 'https://wow.zamimg.com/images/wow/icons/large/inv_engineering_90_blackhole.jpg',
+  description: 'Capture heavy downward momentum. High risk.',
+  strategyType: STRATEGY_TYPE.BEAR,
+  legs: [
+    {
+      action: MACRO_ACTION.BUY,
+      orderClass: ORDER_CLASS.OPTION,
+      optionType: OPTION_TYPE.PUT,
+      optionTranche: -2,
+      quantity: 1,
+      automation: [
+        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.14},
       ],
     },
   ],
 };
 
 export const MACRO_ICE_TICKET: IMacro = {
-  id: '9',
+  id: '10',
   name: 'Ice Ticket',
   icon: 'https://wow.zamimg.com/images/wow/icons/large/inv_10_inscription_darkmooncards_frost_blank.jpg',
   description: 'Capture extreme downward momentum. High risk.',
@@ -191,41 +200,10 @@ export const MACRO_ICE_TICKET: IMacro = {
       action: MACRO_ACTION.BUY,
       orderClass: ORDER_CLASS.OPTION,
       optionType: OPTION_TYPE.PUT,
-      optionTranche: -2,
+      optionTranche: -3,
       quantity: 1,
       automation: [
-        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
-      ],
-    },
-  ],
-};
-
-export const MACRO_BLIZZARD: IMacro = {
-  id: '10',
-  name: 'Blizzard',
-  icon: 'https://wow.zamimg.com/images/wow/icons/large/spell_frost_icestorm.jpg',
-  description:
-    'Create a series of bearish positions to dilute risk while capturing downward momentum. Medium risk.',
-  strategyType: STRATEGY_TYPE.BEAR,
-  legs: [
-    {
-      action: MACRO_ACTION.BUY,
-      orderClass: ORDER_CLASS.OPTION,
-      optionType: OPTION_TYPE.PUT,
-      optionTranche: +2,
-      quantity: 1,
-      automation: [
-        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
-      ],
-    },
-    {
-      action: MACRO_ACTION.BUY,
-      orderClass: ORDER_CLASS.OPTION,
-      optionType: OPTION_TYPE.PUT,
-      optionTranche: -2,
-      quantity: 1,
-      automation: [
-        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.1},
+        {type: AUTOMATION_TYPE.LIMIT_SELL, quantityPercent: 1, goal: 1.14},
       ],
     },
   ],
@@ -234,12 +212,12 @@ export const MACRO_BLIZZARD: IMacro = {
 export const MACROS = {
   [MACRO_FLARE.id]: MACRO_FLARE,
   [MACRO_FIREBALL.id]: MACRO_FIREBALL,
+  [MACRO_FIRE_STORM.id]: MACRO_FIRE_STORM,
   [MACRO_EXPLOSION.id]: MACRO_EXPLOSION,
   [MACRO_FIRE_TICKET.id]: MACRO_FIRE_TICKET,
-  [MACRO_FIRE_STORM.id]: MACRO_FIRE_STORM,
   [MACRO_FROST_BOLT.id]: MACRO_FROST_BOLT,
   [MACRO_ICE_LANCE.id]: MACRO_ICE_LANCE,
+  [MACRO_BLIZZARD.id]: MACRO_BLIZZARD,
   [MACRO_BLACK_HOLE.id]: MACRO_BLACK_HOLE,
   [MACRO_ICE_TICKET.id]: MACRO_ICE_TICKET,
-  [MACRO_BLIZZARD.id]: MACRO_BLIZZARD,
 };
